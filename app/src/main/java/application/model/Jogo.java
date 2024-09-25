34247f879b46e7ded4e4deed579d9191d019391d
Rodrigoalbertopimentel rodrigoalberto <thess-p@hotmail.com>
2024-09-25 23:46:30 +0000

    salvar

--- app/src/main/java/application/model/Jogo.java
+++ app/src/main/java/application/model/Jogo.java
@@ -1,14 +1,5 @@
 package application.model;
 
-import java.uti .HashSet;
-import java.util.Set;
-
-import jakarta.persistence.column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.generetedValue;
-import jakarta.persistence.GenerationType;
-package application.model;
-
 import java.util.HashSet;
 import java.util.Set;
 
